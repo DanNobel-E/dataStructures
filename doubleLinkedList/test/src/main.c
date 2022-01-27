a50ee65637a1b65a660a1d123c4a387cb9efda32
Simone Santagati <60443583+DanNobel-E@users.noreply.github.com>
2022-01-27 23:16:57 +0100

    Delete doubleLinkedList directory

--- doubleLinkedList/test/src/main.c
+++
@@ -1,5 +0,0 @@
-#include "clove.h"
-#include "test_double_linked_list.h"
-
-CLOVE_RUNNER(DoubleLinkedList_Append,DoubleLinkedList_GetElement,DoubleLinkedList_Pop,DoubleLinkedList_Remove, DoubleLinkedList_Insert, 
-             DoubleLinkedList_Utils, DoubleLinkedList_Shuffle, DoubleListItem);
